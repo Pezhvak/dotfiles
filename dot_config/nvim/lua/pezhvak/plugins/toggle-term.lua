50044dd3c03846736fdb10d1ece92d6c986880b5
Pezhvak <pezhvak@imvx.org>
2026-05-11 15:26:20 +0330

    feat(nvim): add OpenCode toggle terminal keymap

--- dot_config/nvim/lua/pezhvak/plugins/toggle-term.lua
+++ dot_config/nvim/lua/pezhvak/plugins/toggle-term.lua
@@ -59,10 +59,29 @@ return {
 			codexTerm:toggle()
 		end
 
+		local opencodeTerm = Terminal:new({
+			cmd = "opencode",
+			hidden = true,
+			direction = "float",
+			close_on_exit = false,
+			title_pos = "center",
+			float_opts = {
+				border = "none",
+				display_name = "OpenCode",
+				width = 160,
+				height = 32,
+			},
+		})
+
+		local function opencode_term_toggle()
+			opencodeTerm:toggle()
+		end
+
 		local function close_all_terminals()
 			lazygit:close()
 			floatTerm:close()
 			codexTerm:close()
+			opencodeTerm:close()
 		end
 
 		vim.api.nvim_create_autocmd("TermClose", {
@@ -78,6 +97,7 @@ return {
 		vim.keymap.set("n", "<leader>tg", lazy_git_toggle, { desc = "Toggle LazyGit", silent = true })
 		vim.keymap.set("n", "<leader>tt", float_term_toggle, { desc = "Toggle floating terminal", silent = true })
 		vim.keymap.set("n", "<leader>tc", codex_term_toggle, { desc = "Toggle Codex terminal", silent = true })
+		vim.keymap.set("n", "<leader>to", opencode_term_toggle, { desc = "Toggle OpenCode terminal", silent = true })
 		vim.keymap.set("t", "<c-w>", close_all_terminals, { desc = "Close all floating terminals", silent = true })
 	end,
 }
