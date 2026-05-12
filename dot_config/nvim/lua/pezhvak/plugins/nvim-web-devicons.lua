91dbe28b78d385732ff0a15723f763d1e7f4e1de
Pezhvak <pezhvak@imvx.org>
2026-05-12 15:58:20 +0330

    fix(nvim): override yaml devicon mapping

---
+++ dot_config/nvim/lua/pezhvak/plugins/nvim-web-devicons.lua
@@ -0,0 +1,9 @@
+return {
+  'nvim-tree/nvim-web-devicons',
+  opts = {
+    override_by_extension = {
+      yaml = { icon = '', color = '#6d8086', name = 'Yaml' },
+      yml = { icon = '', color = '#6d8086', name = 'Yml' },
+    },
+  },
+}
