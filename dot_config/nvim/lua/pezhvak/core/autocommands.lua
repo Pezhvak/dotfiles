5b849aa2e358427ad102ecbcf376521d1e452b29
Pezhvak <pezhvak@imvx.org>
2026-05-12 17:42:01 +0330

    fix(nvim): stabilize markdown and yaml wrap-on-save flow

--- dot_config/nvim/lua/pezhvak/core/autocommands.lua
+++ dot_config/nvim/lua/pezhvak/core/autocommands.lua
@@ -5,38 +5,97 @@
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
 
-vim.api.nvim_create_autocmd('FileType', {
-  desc = 'Enable spell check only for prose-like filetypes',
-  group = vim.api.nvim_create_augroup('pezhvak-prose-spell', { clear = true }),
-  pattern = { 'gitcommit', 'markdown', 'text' },
-  callback = function()
-    vim.opt_local.spell = true
-  end,
+vim.api.nvim_create_autocmd("FileType", {
+	desc = "Enable spell check only for prose-like filetypes",
+	group = vim.api.nvim_create_augroup("pezhvak-prose-spell", { clear = true }),
+	pattern = { "gitcommit", "markdown", "text" },
+	callback = function()
+		vim.opt_local.spell = true
+	end,
 })
 
-vim.api.nvim_create_autocmd('FileType', {
-  desc = 'Wrap prose at 80 columns',
-  group = vim.api.nvim_create_augroup('pezhvak-prose-wrap', { clear = true }),
-  pattern = { 'gitcommit', 'markdown', 'text' },
-  callback = function()
-    vim.opt_local.textwidth = 80
-    vim.opt_local.wrap = true
-    vim.opt_local.linebreak = true
-    vim.opt_local.breakindent = true
-    vim.opt_local.formatoptions:append { 't', 'q' }
-  end,
+vim.api.nvim_create_autocmd("FileType", {
+	desc = "Wrap prose at 80 columns",
+	group = vim.api.nvim_create_augroup("pezhvak-prose-wrap", { clear = true }),
+	pattern = { "gitcommit", "markdown", "text" },
+	callback = function()
+		vim.opt_local.textwidth = 80
+		vim.opt_local.wrap = true
+		vim.opt_local.linebreak = true
+		vim.opt_local.breakindent = true
+		vim.opt_local.formatoptions:append({ "t", "q" })
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	desc = "Hard-wrap markdown paragraphs at 80 columns on save",
+	group = vim.api.nvim_create_augroup("pezhvak-markdown-hard-wrap", { clear = true }),
+	pattern = "*.md",
+	callback = function()
+		local view = vim.fn.winsaveview()
+		local was_modified = vim.bo.modified
+
+		vim.cmd([[silent keepjumps normal! ggVGgq]])
+
+		if not was_modified and vim.bo.modified then
+			vim.bo.modified = false
+		end
+
+		vim.fn.winrestview(view)
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	desc = "Wrap yaml comments at 80 columns",
+	group = vim.api.nvim_create_augroup("pezhvak-yaml-wrap", { clear = true }),
+	pattern = { "yaml" },
+	callback = function()
+		vim.opt_local.textwidth = 80
+		vim.opt_local.formatoptions:append({ "c", "q" })
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	desc = "Hard-wrap yaml comment blocks at 80 columns on save",
+	group = vim.api.nvim_create_augroup("pezhvak-yaml-comment-hard-wrap", { clear = true }),
+	pattern = { "*.yaml", "*.yml" },
+	callback = function()
+		local view = vim.fn.winsaveview()
+		local lnum = 1
+		local line_count = vim.api.nvim_buf_line_count(0)
+
+		while lnum <= line_count do
+			local line = vim.fn.getline(lnum)
+			if line:match("^%s*#") then
+				local start_lnum = lnum
+				repeat
+					lnum = lnum + 1
+					line = vim.fn.getline(lnum)
+				until lnum > line_count or not line:match("^%s*#")
+
+				local end_lnum = lnum - 1
+				vim.cmd(string.format("silent keepjumps normal! %dGV%dGgq", start_lnum, end_lnum))
+			else
+				lnum = lnum + 1
+			end
+
+			line_count = vim.api.nvim_buf_line_count(0)
+		end
+
+		vim.fn.winrestview(view)
+	end,
 })
 
 -- Remove comment leader when inserting a new line
-vim.cmd [[autocmd FileType * set formatoptions-=ro]]
+vim.cmd([[autocmd FileType * set formatoptions-=ro]])
 
 -- Golang auto imports on save
 -- vim.api.nvim_create_autocmd('BufWritePre', {
--- dot_config/nvim/lua/pezhvak/plugins/conform.lua
+++ dot_config/nvim/lua/pezhvak/plugins/conform.lua
@@ -1,44 +1,61 @@
 -- NOTE: Part of LSP Plugins
 return { -- Autoformat
-  'stevearc/conform.nvim',
-  event = { 'BufWritePre' },
-  cmd = { 'ConformInfo' },
-  keys = {
-    {
-      '<leader>f',
-      function()
-        require('conform').format { async = true, lsp_format = 'fallback' }
-      end,
-      mode = '',
-      desc = '[F]ormat buffer',
-    },
-  },
-  opts = {
-    notify_on_error = false,
-    format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
-      if disable_filetypes[vim.bo[bufnr].filetype] then
-        return nil
-      else
-        return {
-          timeout_ms = 500,
-          lsp_format = 'fallback',
-        }
-      end
-    end,
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      markdown = { 'prettierd', 'prettier' },
-      yaml = { 'prettierd', 'prettier', stop_after_first = true },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use 'stop_after_first' to run the first available formatter from the list
-      javascript = { 'prettierd', 'prettier', stop_after_first = true },
-    },
-    formatters = {},
-  },
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end,
+			mode = "",
+			desc = "[F]ormat buffer",
+		},
+	},
+	opts = {
+		notify_on_error = false,
+		format_on_save = function(bufnr)
+			-- Disable "format_on_save lsp_fallback" for languages that don't
+			-- have a well standardized coding style. You can add additional
+			-- languages here or re-enable it for the disabled ones.
+			local disable_filetypes = { c = true, cpp = true }
+			if disable_filetypes[vim.bo[bufnr].filetype] then
+				return nil
+			else
+				return {
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				}
+			end
+		end,
+		formatters_by_ft = {
+			lua = { "stylua" },
+			markdown = { "prettierd_markdown" },
+			yaml = { "prettierd_yaml" },
+			-- Conform can also run multiple formatters sequentially
+			-- python = { "isort", "black" },
+			--
+			-- You can use 'stop_after_first' to run the first available formatter from the list
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+		},
+		formatters = {
+			prettierd_markdown = {
+				inherit = false,
+				command = "prettierd",
+				args = { "$FILENAME" },
+				env = {
+					PRETTIERD_DEFAULT_CONFIG = vim.fn.stdpath("config") .. "/.prettierrc.json",
+				},
+			},
+			prettierd_yaml = {
+				inherit = false,
+				command = "prettierd",
+				args = { "$FILENAME" },
+				env = {
+					PRETTIERD_DEFAULT_CONFIG = vim.fn.stdpath("config") .. "/.prettierrc.json",
+				},
+			},
+		},
+	},
 }
