5fc34aea43265b89c039588b3412e98a933704ce
Pezhvak <pezhvak@imvx.org>
2026-05-12 17:14:41 +0330

    fix(nvim): initialize Copilot Tab mapping before plugin load

--- dot_config/nvim/lua/pezhvak/plugins/copilot.lua
+++ dot_config/nvim/lua/pezhvak/plugins/copilot.lua
@@ -1,4 +1,14 @@
 -- copilot
 return {
-  'github/copilot.vim',
+	"github/copilot.vim",
+	init = function()
+		vim.g.copilot_no_tab_map = true
+		vim.g.copilot_assume_mapped = true
+
+		vim.keymap.set("i", "<Tab>", 'copilot#Accept("\\<Tab>")', {
+			expr = true,
+			replace_keycodes = false,
+			silent = true,
+		})
+	end,
 }
