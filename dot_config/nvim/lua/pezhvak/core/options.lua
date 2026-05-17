5d3071433552d0c3aa340d7c3a2f9d8ae8e7c6a4
Pezhvak <pezhvak@imvx.org>
2026-05-18 00:29:10 +0330

    feat(nvim): default indent to 2 spaces

--- dot_config/nvim/lua/pezhvak/core/options.lua
+++ dot_config/nvim/lua/pezhvak/core/options.lua
@@ -14,10 +14,10 @@ vim.o.numberwidth = 4
 vim.o.ruler = false
 
 -- tabs
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.opt.softtabstop = 4
+vim.opt.softtabstop = 2
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 
