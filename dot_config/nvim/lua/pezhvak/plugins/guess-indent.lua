2aa52eed2da30778f8e92d46466df1fe7933c623
Pezhvak <pezhvak@imvx.org>
2026-05-18 21:02:36 +0330

    fix(nvim): skip guess-indent for filetypes prettier owns

--- dot_config/nvim/lua/pezhvak/plugins/guess-indent.lua
+++ dot_config/nvim/lua/pezhvak/plugins/guess-indent.lua
@@ -1,6 +1,25 @@
 return { -- Detect tabstop and shiftwidth automatically
-  'NMAC427/guess-indent.nvim',
-  config = function()
-    require('guess-indent').setup {}
-  end,
+	"NMAC427/guess-indent.nvim",
+	config = function()
+		require("guess-indent").setup({
+			filetype_exclude = {
+				"netrw",
+				"tutor",
+				"javascript",
+				"javascriptreact",
+				"typescript",
+				"typescriptreact",
+				"json",
+				"jsonc",
+				"css",
+				"scss",
+				"html",
+				"vue",
+				"svelte",
+				"graphql",
+				"markdown",
+				"yaml",
+			},
+		})
+	end,
 }
